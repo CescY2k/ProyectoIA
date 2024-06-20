96f722687e6f005670e65cb6954365f1d379cdf6
César Augusto  Montalván Morales <52120288+CescY2k@users.noreply.github.com>
2024-06-19 19:16:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
 import os
-import keras
-from keras.models import load_model
-import streamlit as st 
+import streamlit as st
 import tensorflow as tf
+from tensorflow.keras.models import load_model
 import numpy as np
 
-st.header('Flower Classification CNN Model')
+# Cargar el modelo
+@st.cache(allow_output_mutation=True)
+def load_my_model():
+    return load_model('Flores_Recog_Model.h5')
+
+model = load_my_model()
+
+# Nombres de las flores
 flower_names = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
 
-model = load_model('Flores_Recog_Model.h5')
+# Función para clasificar imágenes
+def classify_image(image):
+    input_image = tf.image.resize(image, (180, 180))
+    input_image = tf.expand_dims(input_image, axis=0)
+    predictions = model.predict(input_image)
+    score = tf.nn.softmax(predictions[0])
+    return flower_names[np.argmax(score)], 100 * np.max(score)
 
-def classify_images(image_path):
-    input_image = tf.keras.utils.load_img(image_path, target_size=(180,180))
-    input_image_array = tf.keras.utils.img_to_array(input_image)
-    input_image_exp_dim = tf.expand_dims(input_image_array,0)
+# Interfaz de usuario con Streamlit
+st.title('Flower Classification CNN Model')
 
-    predictions = model.predict(input_image_exp_dim)
-    result = tf.nn.softmax(predictions[0])
-    outcome = 'The Image belongs to ' + flower_names[np.argmax(result)] + ' with a score of '+ str(np.max(result)*100)
-    return outcome
+uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
 
-uploaded_file = st.file_uploader('Upload an Image')
 if uploaded_file is not None:
-    with open(os.path.join('upload', uploaded_file.name), 'wb') as f:
-        f.write(uploaded_file.getbuffer())
-    
-    st.image(uploaded_file, width = 200)
+    image = tf.image.decode_image(uploaded_file.read(), channels=3)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
 
-    st.markdown(classify_images(uploaded_file))
-
+    class_name, confidence = classify_image(image)
+    st.write(f"Prediction: {class_name}, Confidence: {confidence:.2f}%")
